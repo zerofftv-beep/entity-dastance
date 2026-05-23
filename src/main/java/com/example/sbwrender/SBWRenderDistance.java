78c05f63f3ff09fc3abebbdd3fb9b5ec6dfa25e6
zerofftv-beep <zerofftv@gmail.com>
2026-05-23 03:04:37 +0300

    Update SBWRenderDistance.java

--- src/main/java/com/example/sbwrender/SBWRenderDistance.java
+++ src/main/java/com/example/sbwrender/SBWRenderDistance.java
@@ -4,16 +4,16 @@ import com.example.sbwrender.command.RenderDistanceCommand;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegisterCommandsEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(SBWRenderDistance.MODID)
 public class SBWRenderDistance {
     public static final String MODID = "sbwrender";
 
     public SBWRenderDistance() {
-        ModLoadingContext.get().registerConfig(ModConfig.Type.SERVER, Config.SPEC);
+        FMLJavaModLoadingContext.get().registerConfig(ModConfig.Type.SERVER, Config.SPEC);
         MinecraftForge.EVENT_BUS.register(this);
     }
 
